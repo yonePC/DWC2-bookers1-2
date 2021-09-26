e415c0947829096f63fa295e44bea34e1294f9e6
yonePC <ys.mailaddress0929@gmail.com>
2021-09-26 13:09:42 +0000

    add homes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'homes#top'
+  # get 'homes/top'
+
+  resources :books, only:[:index, :show, :create, :update, :destroy]
 end
