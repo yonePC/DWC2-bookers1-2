c7f474792c880369846c3963c2b62fb2b4438af3
yonePC <ys.mailaddress0929@gmail.com>
2021-09-27 00:58:34 +0000

    add books/show,edit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,21 +5,36 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def create
     @newbook = Book.new(book_params)
-    @newbook.save
-    redirect_to books_path
+    if @newbook.save
+      redirect_to books_path, notice: "successfully"
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def edit
+    @editbook = Book.find(params[:id])
   end
 
   def update
+    @editbook = Book.find(params[:id])
+    if @editbook.update(book_params)
+      redirect_to book_path(@editbook), notice: "successfully"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
   private
